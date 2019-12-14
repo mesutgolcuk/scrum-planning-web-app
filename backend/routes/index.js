10bdf7e11482d6b174eff71da0182268132d674e
Mesut Gölcük <mesutgolcuk@gmail.com>
2019-12-15 01:05:07 +0300

    Update backend for scrum master panel

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var index = 0;
+var sessionName;
+var voterNum;
+var stories = [];
+
+router.post('/api/add-stories', function(req, res, next) {
+  console.log(req);
+  sessionName = req.body.sessionName;
+  voterNum = req.body.voterNum; 
+  var storiesArr = req.body.stories.split("\n");
+  
+  storiesArr.forEach(story => {
+    stories.push({
+      'id': index,
+      'story': story,
+      'storyPoint': undefined,
+      'status': false
+    });
+    index++;
+  });
+  console.log(sessionName + ',' + voterNum + ',' + stories);
+  res.json({'success': true});
+});
+
+router.get('/api/get-panel-data', function(req, res, next) {
+  res.json({
+    'success': true,
+    'response': {
+      'sessionName': sessionName,
+      'voterNum': voterNum,
+      'stories': stories
+    }
+  });
 });
 
 module.exports = router;
